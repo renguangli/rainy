67f2e289eebb1fc7114361e3b94abd0bba626f2e
renguangli <renguangli@bonc.com.cn>
2022-05-21 23:50:07 +0800

    Add Activate.vue

---
+++ rainy-ui/src/views/user/Activate.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+import { Activate } from '@/api/login'
+<script>
+import { Activate } from '@/api/login'
+export default {
+  name: 'Activate',
+  data () {
+    return {}
+  },
+  created () {
+    this.activeAccount()
+  },
+  methods: {
+    activeAccount () {
+      const token = this.$route.query.token
+      Activate(token).then(res => {
+        if (res.success) {
+          this.$message.info('注册成功：')
+        } else {
+          this.$message.error('激活失败：' + res.message)
+        }
+      })
+    }
+  }
+}
+</script>

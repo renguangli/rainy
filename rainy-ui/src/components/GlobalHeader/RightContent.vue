77d383eb6509463e4c284fcc10f459dafb36146a
renguangli <renguangli@bonc.com.cn>
2022-06-03 16:18:59 +0800

    spring-boot upgrade 2.7.0 Add feedback plugin

--- rainy-ui/src/components/GlobalHeader/RightContent.vue
+++ rainy-ui/src/components/GlobalHeader/RightContent.vue
@@ -1,16 +1,19 @@
 <template>
   <div :class="wrpCls">
+    <button style="padding: 0;border: 0;background: #ffffff;cursor: pointer;" data-feedbackfin-button>反馈</button>
     <notice-icon style="vertical-align: -0.25em;cursor: pointer"></notice-icon>
     <avatar-dropdown :menu="showMenu" :current-user="currentUser" :class="prefixCls" />
     <select-lang :class="prefixCls" />
   </div>
 </template>
-
 <script>
 import NoticeIcon from '@/components/NoticeIcon'
 import AvatarDropdown from './AvatarDropdown'
 import SelectLang from '@/components/SelectLang'
 
+window.feedbackfin = { config: {}, ...window.feedbackfin }
+window.feedbackfin.config.url = '/feedback'
+window.feedbackfin.config.user = { name: '123', email: '123' }
 export default {
   name: 'RightContent',
   components: {
